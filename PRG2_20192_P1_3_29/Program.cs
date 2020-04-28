c66b0439f6dffb5a0ba990d4a539704ea7abf86e
Syamsul Ramadhoni <syamsulramadhoni17@gmail.com>
2020-04-28 13:41:39 +0700

    Initial Commit

---
+++ PRG2_20192_P1_3_29/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarasi
+            int n; //Jumlah pengulangannya
+
+            //Algoritma
+            Console.WriteLine("Program Menulis Syair Lagu Anak Ayam\n"); //Judul program
+            Console.Write("Masukkan nilai N : "); n = Convert.ToInt16(Console.ReadLine());
+            for(int i = n; i >= 1; i--)
+            {
+                if(i==1)
+                {
+                    Console.WriteLine("Anak ayam turun 1, mati satu tinggal induknya");
+                }
+                else
+                {
+                    Console.WriteLine("Anak ayam turun {0}, mati satu tinggal {1}", i, i-1);
+                }
+            }
+        }
+    }
+}
